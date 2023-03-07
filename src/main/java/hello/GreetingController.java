2f2bca88b1f103969462ab15a6dc98450c937aa5
DiegoCruzCuervo <diego.cruz@alumnos.upm.es>
2023-03-07 10:40:17 +0100

    Hola

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -3,7 +3,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-<<<<<<< HEAD
 
 @Controller
 public class GreetingController{
@@ -13,14 +12,3 @@ public class GreetingController{
 		return "greeting";
 	}
 }
-=======
-@Controller
-public class GreetingController {
- @GetMapping("/greeting")
- public String greeting(@RequestParam(name="name", required=false, defaultValue="World")
-String name, Model model) {
- model.addAttribute("name", name);
- return "greeting";
- }
-}
->>>>>>> refs/remotes/origin/master
